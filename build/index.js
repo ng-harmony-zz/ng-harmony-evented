5913aa6103c5203dd5874aef1d2c03138352530a
DI (FH) Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2016-02-25 20:25:20 +0100

    filtering comment nodes ... Bump

--- build/index.js
+++ build/index.js
@@ -49,8 +49,8 @@
                 }
                 if (key.match("::")) {
                     let tokens = key.split("::");
-                    if ((tokens[2] !== undefined && tokens[2] !== null) && !!~tokens[2].indexOf(">")) {
-                        tokens = tokens.splice(0, 2).concat(tokens[0].split(">"));
+                    if ((typeof tokens[2] !== "undefined" && tokens[2] !== null) && !!~tokens[2].indexOf(">")) {
+                        tokens = tokens.splice(3, 0, ...tokens[2].split(">"));
                     }
                     let element = this.$element ? this.$element.context : document.body;
                     for (let [i, el] of (tokens[0] ?
@@ -58,15 +58,15 @@
                             [element].entries())) {
                         ((_i, _el, _fn) => {
                             let __fn = (ev, ...args) => {
-                                if (tokens[2] !== undefined && tokens[2] !== null) {
-                                    if (tokens[3] !== undefined && tokens[3] !== null) {
+                                if (typeof tokens[2] !== "undefined" && tokens[2] !== null) {
+                                    if (typeof tokens[3] !== "undefined" && tokens[3] !== null) {
                                         let __el = ev.currentTarget.parentNode;
                                         while (!zest.matches(__el, tokens[3])) { __el = __el.parentNode; }
-                                        let list = Array.prototype.slice.call(__el.parentNode.childNodes);
+                                        let list = Array.prototype.slice.call(__el.parentNode.children);
                                         this.$scope.i = list.indexOf(__el);
                                     } else {
                                         let __el = ev.currentTarget;
-                                        let list = Array.prototype.slice.call(__el.parentNode.childNodes);
+                                        let list = Array.prototype.slice.call(__el.parentNode.children);
                                         this.$scope.i = list.indexOf(__el);
                                     }
                                 }
